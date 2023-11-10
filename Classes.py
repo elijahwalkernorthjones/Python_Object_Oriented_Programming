71738f56370b553b38e2fd32f03eb547e187c846
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2023-11-10 07:38:44 -0800

    Learning about Python OOP

---
+++ Classes.py
@@ -0,0 +1,19 @@
+from math import sqrt
+from random import randint
+
+class Triangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @classmethod
+    def random(cls):
+        return cls(randint(1,20), randint(1,10))
+
+    def get_hypotenuse(self):
+        return sqrt(self.a ** 2 + self.b ** 2)
+    
+    def get_area(self):
+        return self.a * self.b / 2
+    
+
